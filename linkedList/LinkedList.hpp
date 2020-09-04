01af0864d2914814c5cefcedf6f5bde0bc2ffc4f
Wenxuan Hu <w472h671@ku.edu>
2020-09-04 15:36:28 -0500

    write a for loop

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -44,6 +44,17 @@ bool LinkedList<T>::search(T value) const
 	/** TODO
 		Fix this method
 	*/
+	for(int i=0;i<m-_size;i++)
+	{
+		if(temp->getValue()==value)
+		{
+			isFound=true;
+		}
+		else
+		{
+			temp=temp->getNext();
+		}
+	}
 
 	return(isFound);
 }
